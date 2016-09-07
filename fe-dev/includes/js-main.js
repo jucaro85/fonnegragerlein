20b865a7ae862456bb49d778de2a20cc032cea05
jucaro85 <rondoncardenas@gmail.com>
2016-09-07 11:22:49 -0500

    banner home

--- fe-dev/includes/js-main.js
+++ fe-dev/includes/js-main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
-  $("#galeria").slick();
+  $("#fg-slickHome").slick({
+  	autoplay:true,
+  	fade: true,
+  	arrows:false,
+  	speed: 1400,
+  	autoplaySpeed: 7500,
+  	pauseOnHover:false
+  });
 
 });
