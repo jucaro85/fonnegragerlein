9ed4ec84ed9372407b09148c8eb81fc28c8fa44e
Miguel Rozo <miguelrbackbonetechnology@gmail.com>
2016-09-08 18:22:44 -0500

    added minisite_unidad

--- fe-dev/includes/js-main.js
+++ fe-dev/includes/js-main.js
@@ -10,6 +10,27 @@ function EstatesSlider(){
 	});
 }
 
+function formatNumber(n){ return n.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1.'); }
+
+function dwellingOpsSlider(sclass, minv, maxv, startd, endd, stepd) {
+	$(sclass).each(function(){
+		var $slider = $('.dwellingops-slider', this);
+		$slider.slider({
+			range: true,
+			min: minv,
+			max: maxv,
+			values: [startd,endd],
+			step: stepd,
+			slide: function(e,ui) {
+				$(sclass).find('.dwellingops-sliderMin').text( formatNumber(ui.values[0]) );
+				$(sclass).find('.dwellingops-sliderMax').text( formatNumber(ui.values[1]) );
+			}
+		});
+		$(sclass).find('.dwellingops-sliderMin').text( formatNumber($slider.slider('values', 0)) );
+		$(sclass).find('.dwellingops-sliderMax').text( formatNumber($slider.slider('values', 1)) );
+	});
+}
+
 $(document).ready(function(){
 
   $("#fg-slickHome").slick({
@@ -22,5 +43,9 @@ $(document).ready(function(){
   });
 
   EstatesSlider();
+
+  // Selector, MinValue, MaxValue, MinDefault, MaxDefault, slideJump
+  dwellingOpsSlider('.dwellingops-areaSlider', 0, 600, 0, 300, 50);
+  dwellingOpsSlider('.dwellingops-priceSlider', 0, 4000000000, 0, 2000000000, 100000000);
 
 });
